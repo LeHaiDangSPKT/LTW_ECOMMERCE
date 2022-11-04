115d24c8c9a2ef99f6c12d819fd3faa4b574cd8b
Tran Minh Man <96435164+TranMinhMan040702@users.noreply.github.com>
2022-11-04 10:15:03 +0700

    DAO Prodcut

--- src/main/java/com/mdk/dao/IProductDAO.java
+++ src/main/java/com/mdk/dao/IProductDAO.java
@@ -2,9 +2,14 @@ package com.mdk.dao;
 
 import com.mdk.models.Product;
 
+import java.util.List;
+
 public interface IProductDAO {
     void insert(Product product);
-    void update(int id);
+    void update(Product product);
     void delete(int id);
+    List<Product> getTopSeller(int index);
+    List<Product> findAll();
+    List<Product> findByCategoryId(int categoryId);
 
 }
