2e590557b9088aeb3461c6f156e43332bceee2d8
NguyenHungKhang <104726874+NguyenHungKhang@users.noreply.github.com>
2022-11-04 13:17:35 +0700

    Add files via upload

--- src/main/java/com/mdk/models/Category.java
+++ src/main/java/com/mdk/models/Category.java
@@ -1,4 +1,22 @@
 package com.mdk.models;
 
-public class Category {
+public class Category extends AbstractModel<Delivery> {
+	private String name;
+	private boolean isDelete;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public boolean isDelete() {
+		return isDelete;
+	}
+
+	public void setDelete(boolean isDelete) {
+		this.isDelete = isDelete;
+	}
 }
